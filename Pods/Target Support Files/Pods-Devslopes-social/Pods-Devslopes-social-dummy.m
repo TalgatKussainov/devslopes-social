cd696eb9da2b1798834e1cada6eb20377d82abc0
TalgatKussainov <developer.ktalgat@gmail.com>
2017-01-09 20:06:29 +0400

    Completed setting up Firebase integration

---
+++ Pods/Target Support Files/Pods-Devslopes-social/Pods-Devslopes-social-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Devslopes_social : NSObject
+@end
+@implementation PodsDummy_Pods_Devslopes_social
+@end
---
+++ Pods/Target Support Files/Pods-Devslopes-social/Pods-Devslopes-social-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Devslopes_socialVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Devslopes_socialVersionString[];
+

7fb372f065ddc995b857323a6cf8abed750be5ab
TalgatKussainov <developer.ktalgat@gmail.com>
2017-01-10 17:03:02 +0400

    Sign in ui custom classes complete

---
+++ Devslopes-social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Devslopes-social
+//
+//  Created by Talgat Kussainov on 1/10/17.
+//  Copyright © 2017 Talgat Kussainov. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
